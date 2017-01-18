631ea9420d2d34ae97298c4eb1f9fa8fc5905fa4
Ryan Palo <ryan@thepalos.com>
2017-01-18 23:51:26 +0000

    Finish additional styling

--- js/home.js
+++ js/home.js
@@ -6,8 +6,9 @@ function fixNav() {
     document.body.style.paddingTop = nav.offsetHeight + 'px';
     document.body.classList.add('fixed-nav');
   } else {
-    document.body.classList.remove('fixed-nav');
     document.body.style.paddingTop = 0;
+    document.body.classList.remove('fixed-nav');
+
   }
 }
 
