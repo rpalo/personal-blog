ebcacda7b6b03d8927f82b3c998dc801fb3e9f9c
Ryan Palo <ryan@thepalos.com>
2018-02-03 10:40:47 -0800

    Add cache purging for posts list to deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,13 @@ cd _site
 git add .
 git commit -m "$name"
 git push
+sleep 5
+source secrets.sh
+echo "Purging posts list cache"
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/13f84c3ce59abf8d0e7a6ef82a2d0385/purge_cache" \
+     -H "X-Auth-Email: $email" \
+     -H "X-Auth-Key: $cloudflare_api_key" \
+     -H "Content-Type: application/json" \
+     --data '{"files":["https://assertnotmagic.com/js/posts_data.js"]}'
+
 echo "Complete!"
