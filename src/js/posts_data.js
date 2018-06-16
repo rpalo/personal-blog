6e26740f38b0945b3b0f4d1252bcbc9bbe4377ac
Ryan Palo <ryan@thepalos.com>
2018-06-16 13:28:16 -0700

    Add cloudinary for image optimization

--- src/js/posts_data.js
+++ src/js/posts_data.js
@@ -5,7 +5,7 @@ const posts = [
     {
         title: "{{ post.title }}",
         url: "{{ site.baseurl}}{{ post.url }}",
-        cover: "/img/{{ post.cover_image }}",
+        cover: "{{ 'https://res.cloudinary.com/duninnjce/image/upload/w_300,q_auto,f_auto/' | append: post.cover_image }}",
         tags: "{{ post.tags | join: ', ' }}",
         date: "{{ post.date | date: '%B %d, %Y' }}",
         description: "{{ post.description | strip_html | strip_newlines | escape }}"
