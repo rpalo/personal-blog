d73a1a719f96e3cea30e20983d0a5c51caa162f4
Ryan Palo <ryan@thepalos.com>
2018-09-06 22:43:45 -0700

    Update deploy.sh to be a bit more polished

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
-set -e
-name=$1
+set -o errexit
+
+name="$1"
+
+echo "--- 1/5 Building the site in Production Mode ---"
 export JEKYLL_ENV=production
 bundle exec jekyll build
+
+echo "--- 2/5 Pushing this repo to GitHub ---"
 git add .
 git commit -m "$name"
 git push
+
+echo "--- 3/5 Deploying the built site to GitHub ---"
 cd _site
 git add .
 git commit -m "$name"
 git push
-sleep 10
+
+echo "--- 4/5 Waiting for GitHub to start serving the new build ---"
+for (( i=10; i > 0; i-- )); do
+    printf '\b%d' "$i"
+    sleep 1
+done
+
+echo "--- 5/5 Purging posts list cache ---"
 cd ..
 source secrets.sh
-echo "Purging posts list cache"
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/13f84c3ce59abf8d0e7a6ef82a2d0385/purge_cache" \
      -H "X-Auth-Email: $email" \
      -H "X-Auth-Key: $cloudflare_api_key" \
      -H "Content-Type: application/json" \
      --data '{"files":["https://assertnotmagic.com/js/posts_data.js"]}'
 
-echo ""
-echo "Complete!"
+echo
+echo "--- Complete! ---"
