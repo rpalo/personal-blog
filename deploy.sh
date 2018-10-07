21a3b503ad8a2917093f63badf0232d9972a23b9
Ryan Palo <ryan@thepalos.com>
2018-10-07 09:05:12 -0700

    Update deploy setup with examples for future me on future computers

--- deploy.sh
+++ deploy.sh
@@ -3,28 +3,39 @@ set -o errexit
 
 name="$1"
 
+echo
 echo "--- 1/5 Building the site in Production Mode ---"
+echo
 export JEKYLL_ENV=production
 bundle exec jekyll build
 
+echo
 echo "--- 2/5 Pushing this repo to GitHub ---"
+echo
 git add .
 git commit -m "$name"
 git push
 
+echo
 echo "--- 3/5 Deploying the built site to GitHub ---"
+echo
 cd _site
 git add .
 git commit -m "$name"
 git push
 
+echo
 echo "--- 4/5 Waiting for GitHub to start serving the new build ---"
+echo
 for (( i=10; i > 0; i-- )); do
-    printf '\b%d' "$i"
+    echo -en "\r$i"
     sleep 1
 done
 
+echo
 echo "--- 5/5 Purging posts list cache ---"
+echo
+
 cd ..
 source secrets.sh
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/13f84c3ce59abf8d0e7a6ef82a2d0385/purge_cache" \
@@ -35,3 +46,4 @@ curl -X DELETE "https://api.cloudflare.com/client/v4/zones/13f84c3ce59abf8d0e7a6
 
 echo
 echo "--- Complete! ---"
+echo
